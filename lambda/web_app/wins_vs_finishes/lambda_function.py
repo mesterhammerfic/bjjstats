7271f7c3b3cbe038af7fbb60b60f482f6c26565f
max <maximilian.esterhammer-fic@gamalon.com>
2024-02-06 13:49:12 -0600

    saving progress before pivoting

--- lambda/web_app/wins_vs_finishes/lambda_function.py
+++ lambda/web_app/wins_vs_finishes/lambda_function.py
@@ -13,24 +13,45 @@ connection = psycopg2.connect(
 )
 path = os.path.dirname(__file__)
 env = Environment(loader=FileSystemLoader(path, encoding='utf8'))
-print(path)
+
+
+def get_records():
+    print("getting urls from db")
+    with connection.cursor() as cursor:
+        cursor.execute(
+            """
+            SELECT a.name,
+                   a.id,
+                   CAST(SUM(CASE
+                                WHEN result = 'W' THEN 1
+                                ELSE 0
+                       END) AS DECIMAL) / COUNT(*) AS win_percent,
+                   CAST(SUM(CASE
+                                WHEN method LIKE 'Pts:%' THEN 0
+                                WHEN method IN ('N/A', 'Points') THEN 0
+                                WHEN result = 'W' THEN 1
+                                ELSE 0
+                       END) AS DECIMAL) /
+                   NULLIF(SUM(CASE
+                                  WHEN result = 'W' THEN 1
+                                  ELSE 0
+                       END), 0)                    AS sub_percent
+            FROM athlete a
+                     JOIN performance p on a.id = p.athlete_id
+                     JOIN match m on p.match_id = m.id
+            GROUP BY a.name, a.id;
+            """,
+        )
+        rows = cursor.fetchall()
+    names = [row[0] for row in rows]
+    win_percent = [row[2] for row in rows]
+    finish_percent = [row[3] for row in rows]
+    return names, win_percent, finish_percent
 
 def handler(event, context):
-    def get_urls():
-        print("getting urls from db")
-        with connection.cursor() as cursor:
-            cursor.execute(
-                (
-                    "SELECT url"
-                    " FROM url;"
-                ),
-            )
-            rows = cursor.fetchall()
-        return [row[0] for row in rows]
-    get_urls()
+    names, win_percent, finish_percent = get_records()
 
-    data_canada = px.data.gapminder().query("country == 'Canada'")
-    fig = px.bar(data_canada, x='year', y='pop')
+    fig = px.scatter(x=win_percent, y=finish_percent, text=names)
 
     plotly_jinja_data = {"fig": fig.to_html(full_html=False)}
     # consider also defining the include_plotlyjs parameter to point to an external Plotly.js as described above
